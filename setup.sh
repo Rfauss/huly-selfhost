6135597b1f754d98517a24ebfb1b02e1e8117932
rfauss <randallfauss@gmail.com>
2024-08-29 22:29:28 -0400

    Added caddy support

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export SERVER_ADDRESS="$1"
 echo "Setting Huly Server Address: $SERVER_ADDRESS"
-envsubst < template.conf > nginx.conf
+# envsubst < template.conf > nginx.conf
 envsubst < template.env > .env
 
 ./use-version.sh v0.6.265
